c4f63b0414aa32a1615b0c0f24a8bb8d2e3fb0ce
Miro_up10@itstepru.onmicrosoft.com <Miro_up10@itstepru.onmicrosoft.com>
2021-08-07 09:49:07 +0300

    transfer Function "Sort"

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -146,45 +146,6 @@ template<typename T>void PrintReverse(T arr[], const int n)
 	cout << endl;
 }
 
-
-template<typename T>void Sort(T arr[], const int n)
-{
-	T min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i; j < n; j++)
-		{
-			if (arr[j] < min)
-			{
-				arr[i] = arr[j];
-				arr[j] = min;
-				min = arr[i];
-			}
-		}
-		min = arr[i + 1];
-	}
-}
-template<typename T>void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	T min = arr[0][0];
-	for (int x = 0; x < ROWS; x++)
-	{
-		for (int i = 0; i < COLS; i++)
-		{
-			for (int j = i; j < COLS; j++)
-			{
-				if (arr[x][j] < min)
-				{
-					arr[x][i] = arr[x][j];
-					arr[x][j] = min;
-					min = arr[x][i];
-				}
-			}
-			min = arr[x][i + 1];
-		}
-	}
-}
-
 template<typename T>T Sum(T arr[], const int n)
 {
 	T sum = 0;
@@ -256,4 +217,43 @@ template<typename T>T maxValuein(T arr[ROWS][COLS], const int ROWS, const int CO
 		}
 	}
 	return max;
+}
+
+
+template<typename T>void Sort(T arr[], const int n)
+{
+	T min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)
+		{
+			if (arr[j] < min)
+			{
+				arr[i] = arr[j];
+				arr[j] = min;
+				min = arr[i];
+			}
+		}
+		min = arr[i + 1];
+	}
+}
+template<typename T>void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	T min = arr[0][0];
+	for (int x = 0; x < ROWS; x++)
+	{
+		for (int i = 0; i < COLS; i++)
+		{
+			for (int j = i; j < COLS; j++)
+			{
+				if (arr[x][j] < min)
+				{
+					arr[x][i] = arr[x][j];
+					arr[x][j] = min;
+					min = arr[x][i];
+				}
+			}
+			min = arr[x][i + 1];
+		}
+	}
 }
